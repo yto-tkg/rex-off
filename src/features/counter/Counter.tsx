348fe9f23ca083e83f90d64e5a539d247e01e52c
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-05-23 09:44:14 +0900

    feat: type

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,70 +1,28 @@
-import { useState } from "react";
+import { useSelector } from "react-redux";
+import { decrement, increment } from "./counterSlice";
+import { useAppDispatch, useAppSelector } from "../../hooks";
 
-import { useAppSelector, useAppDispatch } from "../../hooks";
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from "./counterSlice.ts";
-import styles from "./Counter.module.css";
-
-function Counter() {
+export function Counter() {
+  const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
-  const count = useAppSelector(selectCount);
-  const [incrementAmount, setIncrementAmount] = useState("2");
-
-  const incrementValue = Number(incrementAmount) || 0;
 
   return (
     <div>
-      <div className={styles.row}>
+      <div>
         <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
-          +
+          Increment
         </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
+        <span>{count}</span>
         <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
         >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
+          Decrement
         </button>
       </div>
     </div>
   );
 }
-
-export default Counter;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 
-import Counter from "../features/counter/Counter";
-import styles from "../styles/Home.module.css";
 import { Provider } from "react-redux";
-import store from "../app/store";
+import { store } from "../app/store";
 import App from "next/app";
 
 const IndexPage: NextPage = () => {
